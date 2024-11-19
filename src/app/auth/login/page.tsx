43b6ad2f7192d8fc41bc5aec99845249687af79b
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-11-19 19:52:42 +0100

    🐲 Update login form

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/app/auth/components/login-form";
 
 export default function Page() {
   return (
-    <div className="flex h-screen w-full items-center justify-center px-4">
+    <div className="-my-4 flex h-screen w-full items-center justify-center px-4">
       <LoginForm />
     </div>
   );
